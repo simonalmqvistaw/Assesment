d0ae835c6f951b3e9ae052dfcd851fb702c2dc9b
simonalmqvistaw <103564264+simonalmqvistaw@users.noreply.github.com>
2022-04-18 17:01:52 +0200

    Add files via upload

---
+++ dotnet/Contact.cs
@@ -0,0 +1,9 @@
+namespace myAPi {
+
+    public class Contact {
+        public int id {get;set;} = 0;
+        public string firstName {get;set;} = "";
+        public string lastName {get;set;} = "";
+
+    }
+}
